6602efff9e97f8e5a54e256a48b4a5a69ce5a31c
Angelica Lira <angelicavfreitas@gmail.com>
2018-02-09 16:38:59 -0200

    Create Payments

---
+++ src/main/java/Payments.java
@@ -0,0 +1,100 @@
+import br.com.moip.API;
+import br.com.moip.exception.UnexpectedException;
+import br.com.moip.exception.ValidationException;
+import br.com.moip.request.PaymentRequest;
+import br.com.moip.request.ApiDateRequest;
+import br.com.moip.request.InstructionLinesRequest;
+import br.com.moip.request.BoletoRequest;
+import br.com.moip.request.FundingInstrumentRequest;
+import br.com.moip.resource.Order;
+import br.com.moip.resource.Payment;
+import br.com.moip.resource.links.PaymentLinks;
+
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+public class Payments {
+
+
+    private Authorization config;
+
+    private Authorization myAuth = new Authorization();
+
+    private API api = myAuth.startConfig();
+
+    private Scanner input = new Scanner(System.in);
+
+    public  class OrderID extends Orders {
+        private Order orderId;
+        Order order = orderId;
+
+    public Payment createPayments() {
+
+        Payment payment = new Payment();
+
+               
+      try {
+                payment = api.payment().create(new PaymentRequest()
+                        .orderId(String.valueOf(orderId))
+                        .installmentCount(1)
+                        .fundingInstrument(new FundingInstrumentRequest()
+                                .boleto(new BoletoRequest()
+                                        .expirationDate(new ApiDateRequest().date(new GregorianCalendar(2020, Calendar.NOVEMBER, 10).getTime()))
+                                        .logoUri("http://logo.com")
+                                        .instructionLines(new InstructionLinesRequest()
+                                                .first("Primeira linha")
+                                                .second("Segunda linha")
+                                                .third("Terceira linha")
+                                        )
+                                )
+                        )
+                );
+
+            } catch(
+            UnexpectedException e)
+            {
+                //StatusCode >= 500
+            } catch(
+            ValidationException e)
+            {
+                //StatusCode entre 400 e 499 (exceto 401)
+            }
+            return payment;
+
+        }
+
+            public String getPayments() {
+                PaymentLinks paymentlinks = new PaymentLinks();
+
+                String getPayments = String.valueOf(api.payment().get(String.valueOf(paymentlinks)));
+
+
+                Payment getPayment = null;
+                getPayment.getLinks().payBoletoLink();
+
+                Payment getPayment2 = null;
+                getPayment2.getLinks().payBoletoPrintLink();
+
+                System.out.println("\n Pague e Imprima aqui");
+                String link = input.next();
+                String print = input.next();
+                return getPayments();
+            }
+
+        }
+    }
+
+    class Orders extends Order {
+    }
+
+
+
+
+
+
+
+    
+
+
