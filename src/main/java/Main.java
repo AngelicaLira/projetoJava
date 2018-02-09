c2050c97e065b5c0dc0205a1c95642ff275bd6a0
Angelica Lira <angelicavfreitas@gmail.com>
2018-02-09 16:39:34 -0200

    Create Payments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,28 @@ import java.text.ParseException;
 
 public class Main {
 
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args, Payments.OrderID payments) throws ParseException {
 
         Customers customer = new Customers();
 
        System.out.println(customer.createCustomers().getOwnId());
 
 
-            /* verificar onde colocar o try catch*/
+       //* verificar onde colocar o try catch*//*
 
-            System.out.println(customer.getCustomer());
-        }
+      System.out.println(customer.getCustomer());
+
+
+        Payments payment = new Payments();
+
+        System.out.println(payments.createPayments());
+        System.out.println(payments.getPayments());
+
+        Payments2 cc = new Payments2();
+
+        System.out.println(payments.createPayments());
+
+
     }
+}
+
