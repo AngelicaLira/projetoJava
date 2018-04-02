8a5053075cb21da099725c62596784619bc6caf4
Angelica Lira <angelicavfreitas@gmail.com>
2018-04-02 13:27:43 -0300

    refactor(refunds)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,47 +6,19 @@ public class Main {
 
     public static void main(String[] args) throws ParseException {
 
-        //    Customers customer = new Customers();
 
-//       System.out.println(customer.createCustomers().getOwnId());
         /**
          * reembolso cartão
          */
-       Refunds refund = new Refunds();
+        Refunds refund = new Refunds();
         System.out.println(refund.createRefunds());
         /**
          * reembolso boleto
          */
 
-        RefundBoleto refund = new RefundBoleto();
-        System.out.println(refund.createRefundBoleto());
-
-        Escrows escrow = new Escrows();
-        System.out.println(escrow.toString());
-
-        //* verificar onde colocar o try catch*//*
-/*
-      Multiorders multiorder = new Multiorders();
-
-      System.out.println(multiorder.createMultiorder().getId());
-       //System.out.println(multiorder.getMultiorder());
-
-
-      Multipayments multipayments = new Multipayments();
-
-      System.out.println(multipayments.createMultipayment());
-
-       //System.out.println(multipayment.getMultipayment());
-
-
-
-
-
-    }
-
-    private static boolean escrow() {
-        return true;
-    }*/
+        RefundBoleto refunds = new RefundBoleto();
+        System.out.println(refunds.createRefundBoleto());
     }
 }
+
 
